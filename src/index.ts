d7fd084e5b591c4e371f4f4064dbe70198716510
Lofty | Brambles <prantikdas2004@gmail.com>
2022-08-25 01:53:28 +0530

    🎨 style | Adds eslint, prettier, husky, lint-staged

--- src/index.ts
+++ src/index.ts
@@ -3,52 +3,58 @@
 import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
 import simpleGit from "simple-git";
+import chalk from "chalk";
 import options from "./data/options.json" assert { type: "json" };
 
 const getDetails = async () => {
-  const type = await inquirer.prompt({
-    name: "type",
-    type: "list",
-    message: "Please select the type of your commit —",
-    choices: () => {
-      const opts = Object.entries(options.types);
-      const pad = opts
-        .map((ele) => ele[0].length)
-        .reduce((p, n) => Math.max(p, n));
-      return opts.map((ele) => ({
-        name: `${ele[0].padEnd(pad)} — ${ele[1].emoji} ${ele[1].description}`,
-        value: `${ele[1].emoji} ${ele[0]}`,
-      }));
-    },
-  });
+	const type = await inquirer.prompt({
+		name: "type",
+		type: "list",
+		// prettier-ignore
+		message: `Please ${chalk.underline("select the type")} of your commit •`,
+		choices: () => {
+			const opts = Object.entries(options.types);
+			const pad = opts
+				.map(ele => ele[0].length)
+				.reduce((p, n) => Math.max(p, n));
+			return opts.map(ele => ({
+				name: `${ele[0].padEnd(pad)} — ${ele[1].emoji} ${
+					ele[1].description
+				}`,
+				value: `${ele[1].emoji} ${ele[0]}`,
+			}));
+		},
+	});
 
-  const scope = await inquirer.prompt({
-    name: "scope",
-    type: "input",
-    message: `Please enter a commit scope, if none, press ↳ Enter to continue —`,
-  });
+	const scope = await inquirer.prompt({
+		name: "scope",
+		type: "input",
+		message: `Please enter a commit scope, if none, press ↳ Enter to continue •`,
+	});
 
-  const imp = await inquirer.prompt({
-    name: "important",
-    type: "confirm",
-    message: "Is this commit important or breaking? [ Default: No ] —",
-    default: false,
-  });
+	const imp = await inquirer.prompt({
+		name: "important",
+		type: "confirm",
+		message: "Is this commit important or breaking? [ Default: No ] •",
+		default: false,
+	});
 
-  const message = await inquirer.prompt({
-    name: "message",
-    type: "input",
-    message: "Please enter a commit message —",
-  });
+	const message = await inquirer.prompt({
+		name: "message",
+		type: "input",
+		message: "Please enter a commit message •",
+	});
 
-  return [type.type, scope.scope, imp.important, message.message];
+	return [type.type, scope.scope, imp.important, message.message];
 };
 
 const main = async () => {
-  console.clear();
-  const details = await getDetails();
-  const spinner = createSpinner("Saving your commit...").start();
-  const resp = simpleGit().commit("");
+	console.clear();
+	console.log();
+	const details = await getDetails();
+	const spinner = createSpinner("Saving your commit...").start();
+	await simpleGit().commit(details);
+	spinner.success();
 };
 
 main();
